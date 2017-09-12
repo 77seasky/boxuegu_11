4756520f33ba1caaf0300147ddb172132fc3a73c
77seasky <997236939@qq.com>
2017-09-12 17:11:26 +0800

    添加讲师功能

--- src/js/teacher/add.js
+++ src/js/teacher/add.js
@@ -1,2 +1,10 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+
+
+$('#teacher_add_form').ajaxForm(function(data) {
+    if (data.code == 200) {
+        alert('添加讲师成功');
+    }
+})
